fff1868f32d9425cf35b247f310957d11938f463
gaabrielgr <gabrielgr.ti@gmail.com>
2022-04-01 23:24:07 -0300

    enviando cabecalho

---
+++ src/components/header/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul>
+        <MenuItem />
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
