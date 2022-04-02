4fffba6bf53be699e8956e9b1ec9c153c1644f35
RafaelSantini23 <rafaelfarias.santini14@gmail.com>
2022-04-01 23:01:12 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./pages/Home";
 import ContextApiProvider from "./context/ContextApi";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <ContextApiProvider>
+        <Routes>
+
+        </Routes>
         <Home />
       </ContextApiProvider>
+      </BrowserRouter>
     </div>
   );
 }
