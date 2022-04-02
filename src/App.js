75c5528bbe51a682cc831ce7c47438bcb446798a
RafaelSantini23 <rafaelfarias.santini14@gmail.com>
2022-04-02 00:24:04 -0300

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import ContextApiProvider from "./context/ContextApi";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Details from "./pages/Details";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <ContextApiProvider>
         <Routes>
             <Route exact path="/nyttop" element={<Home />} />
+            <Route path='/Details' element={<Details />} >
+                <Route path=':id' element={<Details />} />
+            </Route>
         </Routes>
         <Home />
       </ContextApiProvider>
